1569b50682c2a245e8da3e9c1fe81cafeadeab52
zakirhussainb <zakirhussainb2693@gmail.com>
2017-12-08 23:47:43 +0530

    ReverseAnumber-20171208-001

---
+++ src/java2Novice/ReverseAnumber.java
@@ -0,0 +1,23 @@
+package java2Novice;
+import java.util.*;
+class ReverseAnumber
+{
+	/* Write a program to reverse a number. */
+	
+	public static void main(String args[])
+	{
+		System.out.println("Enter the no. to be reversed ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		int reversedNumber = 0;
+		
+		while ( num != 0 )
+		{
+			reversedNumber = (reversedNumber * 10) + (num % 10);
+			num /= 10;
+		}
+		System.out.println("The reversed no. is " + reversedNumber);
+		sc.close();
+	}
+};
