de28ff8961fa6827392dbe4a352c701fd5209ded
zakirhussainb <zakirhussainb2693@gmail.com>
2017-12-09 00:07:59 +0530

    PrintFibonacciSeries

---
+++ src/java2Novice/PrintFibonacciSeries.java
@@ -0,0 +1,20 @@
+package java2Novice;
+class PrintFibonacciSeries
+{
+	/* Write a program to print fibonacci series. */
+	
+	public static void main(String args[])
+	{
+		int n1=0,n2=1,n3=0;
+		
+		System.out.print(n1 + " " + n2);
+		
+		for ( int i = 2; i <=10; i++ )
+		{
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+			System.out.print(" " + n3);			
+		}		
+	}
+};
